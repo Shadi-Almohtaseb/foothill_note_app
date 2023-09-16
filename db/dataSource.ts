1c8cff92fdc72c8fa2593d65506c26dc3338ea8e
Shadi-Almohtaseb <shadi876543211@gmail.com>
2023-09-16 15:33:44 +0300

    test mysql server1

--- db/dataSource.ts
+++ db/dataSource.ts
@@ -3,11 +3,12 @@ import { Note } from "./entities/Note.js";
 
 const dataSource = new DataSource({
     type: 'mysql',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQL_ADDON_HOST,
+    port: Number(process.env.MYSQL_ADDON_PORT),
+    username: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    url: process.env.MYSQL_ADDON_URI,
     entities: [
         Note
     ],
